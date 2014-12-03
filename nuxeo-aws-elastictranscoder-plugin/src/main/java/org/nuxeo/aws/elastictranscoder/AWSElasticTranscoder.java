e042185762cbfbb3f6013b97438a541694fb59da
Thibaud Arguillere <thibarg.web@free.fr>
2014-12-03 17:34:05 -0500

    first commit

---
+++ nuxeo-aws-elastictranscoder-plugin/src/main/java/org/nuxeo/aws/elastictranscoder/AWSElasticTranscoder.java
@@ -0,0 +1,32 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thibaud Arguillere
+ */
+
+package org.nuxeo.aws.elastictranscoder;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class AWSElasticTranscoder {
+
+    private static final Log log = LogFactory.getLog(AWSElasticTranscoder.class);
+
+    public AWSElasticTranscoder() {
+
+    }
+    
+}
