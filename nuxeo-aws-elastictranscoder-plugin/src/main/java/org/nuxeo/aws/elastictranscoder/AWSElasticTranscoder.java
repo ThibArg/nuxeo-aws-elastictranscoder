894299b2af98fc2f95bc1c7e73d48ee8d4b8d1dc
Thibaud Arguillere <thibarg.web@free.fr>
2014-12-05 16:12:14 -0500

    Work in progress

--- nuxeo-aws-elastictranscoder-plugin/src/main/java/org/nuxeo/aws/elastictranscoder/AWSElasticTranscoder.java
+++ nuxeo-aws-elastictranscoder-plugin/src/main/java/org/nuxeo/aws/elastictranscoder/AWSElasticTranscoder.java
@@ -18,32 +18,58 @@
 package org.nuxeo.aws.elastictranscoder;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.platform.picture.api.BlobHelper;
 import org.nuxeo.runtime.api.Framework;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+/**
+ * First draft uses polling to read job's status, this is good enough for first
+ * POC, but be warned that AWS doc explicitly states that:
+ * <p>
+ * <quote> If you poll the Elastic Transcoder's ReadJob API to track job status,
+ * you need to continuously call ReadJob on every submitted job. This
+ * methodology cannot scale as the number of transcode jobs increases. To solve
+ * this problem, Elastic Transcoder can publish notifications to Amazon SNS
+ * which provides an event-driven mechanism for tracking job status. </quote>
+ * 
+ *
+ * @since TODO
+ */
 public class AWSElasticTranscoder {
 
     private static final Log log = LogFactory.getLog(AWSElasticTranscoder.class);
 
     // The keys in nuxeo.conf
-    public static final String CONF_AWS_KEY_ACCESS = "aws.access.key";
+    public static final String CONF_AWS_KEY_ACCESS = "aws.transcoder.key";
 
-    public static final String CONF_AWS_KEY_SECRET = "aws.secret.access.key";
+    public static final String CONF_AWS_KEY_SECRET = "aws.transcoder.secret";
 
-    protected static String awsKeyAccess;
+    protected static String awsAccessKeyId;
 
     protected static String awsSecretAccessKey;
 
-    protected static String awsFinalKeyBase64;
+    protected static int awsKeysCheckedStatus = -1;
 
-    protected static boolean awsKeysChecked = false;
+    protected boolean isRunning = true;
 
     protected Blob blob;
+    
+    protected File fileOfBlob;
 
     protected String preset;
 
@@ -55,21 +81,42 @@ public class AWSElasticTranscoder {
 
     protected String awsJobId;
 
-    public AWSElasticTranscoder(Blob inBlob, String inPreset) {
-        
+    protected AmazonS3 amazonS3;
+
+    protected AWSCredentialsProvider awsCredentialsProvider;
+
+    public AWSElasticTranscoder(Blob inBlob, String inPreset,
+            String inInputBucket, String inOutputBucket, String inPipeline) {
+
         loadAccessKeysFromConf();
-        if (awsFinalKeyBase64 == null || awsFinalKeyBase64.isEmpty()) {
+        if (awsKeysCheckedStatus != 1) {
             throw new ClientException(
-                    "AWS keys are missing or invalid. Are they correctly set-up in nuxeo.conf?");
+                    "AWS Access Key ID/Secret Access Key are missing or invalid. Are they correctly set-up in nuxeo.conf or as System variables?");
         }
 
         blob = inBlob;
+        fileOfBlob = BlobHelper.getFileFromBlob(blob);
+        if(fileOfBlob == null) {
+            // TODO
+            // . . . create a temp file instead of giving up . . .
+            throw new RuntimeException("Cannot get a File from this blob");
+        }
+        
         preset = inPreset;
-
-        // etc.
+        inputS3Bucket = inInputBucket;
+        outputS3Bucket = inOutputBucket;
+        pipeline = inPipeline;
+        
+        // THIBAUD => PAS SUR QUE CA MARCHE
+        awsCredentialsProvider = new ProfileCredentialsProvider( awsAccessKeyId, awsSecretAccessKey);
+        amazonS3 = new AmazonS3Client(awsCredentialsProvider);
     }
 
     public void transcode() {
+
+        isRunning = true;
+        
+        sendFile();
 
         // Send the file to the s3 inputS3Bucket
         // . . .
@@ -77,30 +124,75 @@ public class AWSElasticTranscoder {
         // Start the job
         // awsJobId = . . .
 
+        // Wait completion
+        // . . .
+
+        isRunning = false;
+
     }
 
     public boolean done() {
 
-        return false;
+        return !isRunning;
     }
 
     public File getTranscodedFile() {
 
-        return null; // the transcoded file
+        return null;
+    }
+
+    protected void sendFile() {
+        
+        try {
+            
+            System.out.println("Uploading a new object to S3 from a file\n");
+            amazonS3.putObject(new PutObjectRequest(
+                    inputS3Bucket, "toto", fileOfBlob));
+            
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " +
+                    "means your request made it " +
+                    "to Amazon S3, but was rejected with an error response" +
+                    " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " +
+                    "means the client encountered " +
+                    "an internal error while trying to " +
+                    "communicate with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+
     }
 
     protected synchronized void loadAccessKeysFromConf() {
 
-        if (!awsKeysChecked) {
-            awsKeyAccess = Framework.getProperty(CONF_AWS_KEY_ACCESS);
+        if (awsKeysCheckedStatus == -1) {
+            awsAccessKeyId = Framework.getProperty(CONF_AWS_KEY_ACCESS);
             awsSecretAccessKey = Framework.getProperty(CONF_AWS_KEY_SECRET);
 
-            awsKeysChecked = true;
-            awsFinalKeyBase64 = null;
-            if (awsKeyAccess != null && awsSecretAccessKey != null) {
-                String toEncode = awsKeyAccess + ":" + awsSecretAccessKey;
-                awsFinalKeyBase64 = new String(
-                        Base64.encodeBase64(toEncode.getBytes()));
+            // Fallback if the keys are not here
+            if (awsAccessKeyId == null || awsAccessKeyId.isEmpty()) {
+                awsAccessKeyId = System.getenv("AWS_ACCESS_KEY_ID");
+            }
+            if (awsSecretAccessKey == null || awsSecretAccessKey.isEmpty()) {
+                awsSecretAccessKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+            }
+
+            if (awsAccessKeyId == null || awsAccessKeyId.isEmpty()) {
+                awsAccessKeyId = System.getenv("AWS_ACCESS_KEY_ID");
+            }
+
+            if (awsSecretAccessKey == null || awsSecretAccessKey.isEmpty()
+                    || awsAccessKeyId == null || awsAccessKeyId.isEmpty()) {
+                awsKeysCheckedStatus = 0;
+            } else {
+                awsKeysCheckedStatus = 1;
             }
         }
     }
--- nuxeo-aws-elastictranscoder-plugin/src/test/java/org/nuxeo/aws/elastictranscoder/test/AWSElasticTranscoderTest.java
+++ nuxeo-aws-elastictranscoder-plugin/src/test/java/org/nuxeo/aws/elastictranscoder/test/AWSElasticTranscoderTest.java
@@ -30,58 +30,72 @@ import com.google.inject.Inject;
 @Deploy({ "nuxeo-aws-elastictranscoder" })
 public class AWSElasticTranscoderTest {
 
-	protected static final String VIDEO_MP4 = "files/a.mp4";
-	
-	// iPhone 5, iPhone 4S, iPad 4G and 3G, iPad mini, Samsung Galaxy
-			// S2/S3/Tab 2 => 1351620000001-100020
-	protected static final String PRESET_iPHONE_5 = "1351620000001-100020";
+    protected static final String VIDEO_MP4 = "files/a.mp4";
 
-	@Inject
-	CoreSession coreSession;
+    // These buckets are created with the AWS nuxeo presales credentials
+    // The keys are set in the nuxeo server configuraiton file.
+    protected static String INTPUTBUCKET = "nuxeo-transcoding-input";
 
-	@Before
-	public void setup() {
+    protected static String OUTPUTBUCKET = "nuxeo-transcoding-output";
 
-	}
+    protected static String PIPELINE = "nuxeo-transcoding-pipeline";
 
-	@After
-	public void cleanup() {
+    // Presets available in US Region East:
+    // https://console.aws.amazon.com/elastictranscoder/home?region=us-east-1#presets:
+    // Web: Facebook, SmugMug, Vimeo, YouTube
+    protected static final String PRESET_WEB = "1351620000001-100070";
 
-	}
+    // iPhone 5, iPhone 4S, iPad 4G and 3G, iPad mini, Samsung Galaxy S2/S3/Tab
+    // 2
+    protected static final String PRESET_iPHONE_5 = "1351620000001-100020";
 
-	@Test
-	public void testTranscodeVideoToIPhone5() throws Exception {
+    @Inject
+    CoreSession coreSession;
 
-		File f = FileUtils.getResourceFileFromContext(VIDEO_MP4);
-		FileBlob fb = new FileBlob(f);
-		
-		AWSElasticTranscoder transcoder = new AWSElasticTranscoder(fb, PRESET_iPHONE_5);
-		
-		transcoder.transcode();
-		
-		// wait until it's done
-		// do {...} while(!transcoder.done()) 
-		
-		File result = transcoder.getTranscodedFile();
-		assertNotNull(result);
-		
-		// Check it is a valid iPhone5 video
-		// . . .
-	}
-	
-	@Test
-	public void testConverter() throws Exception {
-		// Here we test a VideoConversionConverter
-		
-		// create a Video document...
-		
-		// add the binary...
-		
-		// save...
-		
-		// Wait...
-		
-		// Check the transcoded video(s) is/are available
-	}
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void cleanup() {
+
+    }
+
+    @Test
+    public void testTranscodeVideoToIPhone5() throws Exception {
+
+        File f = FileUtils.getResourceFileFromContext(VIDEO_MP4);
+        FileBlob fb = new FileBlob(f);
+
+        AWSElasticTranscoder transcoder = new AWSElasticTranscoder(fb,
+                PRESET_iPHONE_5, INTPUTBUCKET, OUTPUTBUCKET, PIPELINE);
+
+        transcoder.transcode();
+
+        // wait until it's done
+        // do {...} while(!transcoder.done())
+
+        File result = transcoder.getTranscodedFile();
+        assertNotNull(result);
+
+        // Check it is a valid iPhone5 video
+        // . . .
+    }
+
+    @Test
+    public void testConverter() throws Exception {
+        // Here we test a VideoConversionConverter
+
+        // create a Video document...
+
+        // add the binary...
+
+        // save...
+
+        // Wait...
+
+        // Check the transcoded video(s) is/are available
+    }
 
 }
