d231cdd92bbded473c78861d0c14fe70b2fcc101
Thibaud Arguillere <thibarg.web@free.fr>
2014-12-07 09:16:10 -0500

    Format

--- nuxeo-aws-elastictranscoder-plugin/src/main/java/org/nuxeo/aws/elastictranscoder/notification/JobStatusNotification.java
+++ nuxeo-aws-elastictranscoder-plugin/src/main/java/org/nuxeo/aws/elastictranscoder/notification/JobStatusNotification.java
@@ -28,10 +28,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *
  * @since 7.1
  */
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JobStatusNotification {
 
-    @JsonIgnoreProperties(ignoreUnknown=true)
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class JobInput {
         private String key;
 
@@ -48,65 +48,69 @@ public class JobStatusNotification {
             return "JobInput [key=" + key + "]";
         }
     }
-    
-    @JsonIgnoreProperties(ignoreUnknown=true)
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class JobOutput {
         private String id;
+
         private String presetId;
+
         private String key;
+
         private String status;
 
         private String statusDetail;
+
         private int errorCode;
-        
+
         public String getId() {
             return id;
         }
-        
+
         public void setId(String id) {
             this.id = id;
         }
-        
+
         public String getPresetId() {
             return presetId;
         }
-        
+
         public void setPresetId(String presetId) {
             this.presetId = presetId;
         }
-        
+
         public String getKey() {
             return key;
         }
-        
+
         public void setKey(String key) {
             this.key = key;
         }
-        
+
         public String getStatus() {
             return status;
         }
-        
+
         public void setStatus(String status) {
             this.status = status;
         }
-        
+
         public String getStatusDetail() {
             return statusDetail;
         }
-        
+
         public void setStatusDetail(String statusDetail) {
             this.statusDetail = statusDetail;
         }
-        
+
         public int getErrorCode() {
             return errorCode;
         }
-        
+
         public void setErrorCode(int errorCode) {
             this.errorCode = errorCode;
         }
-        
+
         @Override
         public String toString() {
             return "JobOutput [id=" + id + ", presetId=" + presetId + ", key="
@@ -114,92 +118,98 @@ public class JobStatusNotification {
                     + statusDetail + ", errorCode=" + errorCode + "]";
         }
     }
-    
+
     public static enum JobState {
-        PROGRESSING,
-        COMPLETED,
-        ERROR;
-        
+        PROGRESSING, COMPLETED, ERROR;
+
         public boolean isTerminalState() {
-            return this.equals(JobState.COMPLETED) || this.equals(JobState.ERROR);
+            return this.equals(JobState.COMPLETED)
+                    || this.equals(JobState.ERROR);
         }
     }
-    
+
     private static final ObjectMapper mapper = new ObjectMapper();
-    
+
     private JobState state;
+
     private int errorCode;
+
     private String version;
+
     private String jobId;
+
     private String pipelineId;
+
     private JobInput input;
+
     private String outputKeyPrefix;
+
     private List<JobOutput> outputs;
-    
+
     public JobState getState() {
         return state;
     }
-    
+
     public void setState(JobState state) {
         this.state = state;
     }
-    
+
     public int getErrorCode() {
         return errorCode;
     }
-    
+
     public void setErrorCode(int errorCode) {
         this.errorCode = errorCode;
     }
-    
+
     public String getVersion() {
         return version;
     }
-    
+
     public void setVersion(String version) {
         this.version = version;
     }
-    
+
     public String getJobId() {
         return jobId;
     }
-    
+
     public void setJobId(String jobId) {
         this.jobId = jobId;
     }
-    
+
     public String getPipelineId() {
         return pipelineId;
     }
-    
+
     public void setPipelineId(String pipelineId) {
         this.pipelineId = pipelineId;
     }
-    
+
     public JobInput getInput() {
         return input;
     }
-    
+
     public void setInput(JobInput input) {
         this.input = input;
     }
-    
+
     public String getOutputKeyPrefix() {
         return outputKeyPrefix;
     }
-    
+
     public void setOutputKeyPrefix(String outputKeyPrefix) {
         this.outputKeyPrefix = outputKeyPrefix;
     }
-    
+
     public List<JobOutput> getOutputs() {
         return outputs;
     }
-    
+
     public void setOutputs(List<JobOutput> outputs) {
         this.outputs = outputs;
     }
-    
+
     @Override
     public String toString() {
         return "JobStatusNotification [state=" + state + ", errorCode="
@@ -209,7 +219,8 @@ public class JobStatusNotification {
                 + outputs + "]";
     }
 
-    public static JobStatusNotification valueOf(String value) throws IOException {
+    public static JobStatusNotification valueOf(String value)
+            throws IOException {
         return mapper.readValue(value, JobStatusNotification.class);
     }
 }
--- nuxeo-aws-elastictranscoder-plugin/src/main/java/org/nuxeo/aws/elastictranscoder/notification/SqsQueueNotificationWorker.java
+++ nuxeo-aws-elastictranscoder-plugin/src/main/java/org/nuxeo/aws/elastictranscoder/notification/SqsQueueNotificationWorker.java
@@ -41,53 +41,59 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class SqsQueueNotificationWorker implements Runnable {
 
     private static final int MAX_NUMBER_OF_MESSAGES = 5;
+
     private static final int VISIBILITY_TIMEOUT = 15;
+
     private static final int WAIT_TIME_SECONDS = 15;
+
     private static final ObjectMapper mapper = new ObjectMapper();
-    
+
     private AmazonSQS amazonSqs;
+
     private String queueUrl;
+
     private List<JobStatusNotificationHandler> handlers;
-    
+
     private volatile boolean shutdown = false;
-    
+
     public SqsQueueNotificationWorker(AmazonSQS amazonSqs, String queueUrl) {
         this.amazonSqs = amazonSqs;
         this.queueUrl = queueUrl;
         this.handlers = new ArrayList<JobStatusNotificationHandler>();
     }
-    
-    public void addHandler(JobStatusNotificationHandler jobStatusNotificationHandler) {
-        synchronized(handlers) {
+
+    public void addHandler(
+            JobStatusNotificationHandler jobStatusNotificationHandler) {
+        synchronized (handlers) {
             this.handlers.add(jobStatusNotificationHandler);
         }
     }
-    
-    public void removeHandler(JobStatusNotificationHandler jobStatusNotificationHandler) {
-        synchronized(handlers) {
+
+    public void removeHandler(
+            JobStatusNotificationHandler jobStatusNotificationHandler) {
+        synchronized (handlers) {
             this.handlers.remove(jobStatusNotificationHandler);
         }
     }
-    
+
     @Override
     public void run() {
-        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest()
-            .withQueueUrl(queueUrl)
-            .withMaxNumberOfMessages(MAX_NUMBER_OF_MESSAGES)
-            .withVisibilityTimeout(VISIBILITY_TIMEOUT)
-            .withWaitTimeSeconds(WAIT_TIME_SECONDS);
-        
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest().withQueueUrl(
+                queueUrl).withMaxNumberOfMessages(MAX_NUMBER_OF_MESSAGES).withVisibilityTimeout(
+                VISIBILITY_TIMEOUT).withWaitTimeSeconds(WAIT_TIME_SECONDS);
+
         while (!shutdown) {
-            // Long pole the SQS queue.  This will return as soon as a message
+            // Long pole the SQS queue. This will return as soon as a message
             // is received, or when WAIT_TIME_SECONDS has elapsed.
-            List<Message> messages = amazonSqs.receiveMessage(receiveMessageRequest).getMessages();
+            List<Message> messages = amazonSqs.receiveMessage(
+                    receiveMessageRequest).getMessages();
             if (messages == null) {
                 // If there were no messages during this poll period, SQS
-                // will return this list as null.  Continue polling.
+                // will return this list as null. Continue polling.
                 continue;
             }
-            
-            synchronized(handlers) {
+
+            synchronized (handlers) {
                 for (Message message : messages) {
                     try {
                         // Parse notification and call handlers.
@@ -96,18 +102,25 @@ public class SqsQueueNotificationWorker implements Runnable {
                             handler.handle(notification);
                         }
                     } catch (IOException e) {
-                        System.out.println("Failed to convert notification: " + e.getMessage());
+                        System.out.println("Failed to convert notification: "
+                                + e.getMessage());
                     }
-                    
+
                     // Delete the message from the queue.
-                    amazonSqs.deleteMessage(new DeleteMessageRequest().withQueueUrl(queueUrl).withReceiptHandle(message.getReceiptHandle()));
+                    amazonSqs.deleteMessage(new DeleteMessageRequest().withQueueUrl(
+                            queueUrl).withReceiptHandle(
+                            message.getReceiptHandle()));
                 }
             }
         }
     }
-    
-    private JobStatusNotification parseNotification(Message message) throws IOException {
-        SNSNotification<JobStatusNotification> notification = mapper.readValue(message.getBody(), new TypeReference<SNSNotification<JobStatusNotification>>() {});
+
+    private JobStatusNotification parseNotification(Message message)
+            throws IOException {
+        SNSNotification<JobStatusNotification> notification = mapper.readValue(
+                message.getBody(),
+                new TypeReference<SNSNotification<JobStatusNotification>>() {
+                });
         return notification.getMessage();
     }
 
